6e69628f336e98f7f77dc536e7f26a1989c81e2a
athulak <athullive@gmail.com>
2020-08-04 13:41:38 +0200

    full name to user session

--- application/controllers/Login.php
+++ application/controllers/Login.php
@@ -34,6 +34,7 @@ class Login extends CI_Controller {
                 $session = array(
                     'cid' => $data['cust_id'],
                     'cname' => $data['first_name'],
+                    'fullname' => $data['first_name']." ".$data['last_name'],
                     'email' => $data['email'],
                     'userType' => 'user'
                 );
