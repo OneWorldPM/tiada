47f1b713ae02bbd8d74f134f306d9be2b647454a
vishalsavaliya <vishalsavaliya3@gmail.com>
2020-08-11 21:55:31 +0530

    Tiada Member Login Update

--- application/controllers/Login.php
+++ application/controllers/Login.php
@@ -386,4 +386,199 @@ class Login extends CI_Controller {
         }
     }
 
+    function tiada_authentication() {
+        $post = $this->input->post();
+        if (!empty($post)) {
+            $curl = curl_init();
+            curl_setopt_array($curl, array(
+                CURLOPT_URL => "https://secure.membershipsoftware.org/tiadasecure/api/AuthenticateUser/?securityKey=4A17DC6DF22F45B7AAF5A0554FD447&username=" . $post['tiada_email'] . "&password=" . $post['tiada_password'],
+                CURLOPT_RETURNTRANSFER => true,
+                CURLOPT_ENCODING => "",
+                CURLOPT_MAXREDIRS => 10,
+                CURLOPT_TIMEOUT => 0,
+                CURLOPT_FOLLOWLOCATION => true,
+                CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+                CURLOPT_CUSTOMREQUEST => "GET"
+            ));
+            $response = curl_exec($curl);
+            curl_close($curl);
+            $xml = simplexml_load_string($response);
+            $json = json_encode($xml);
+            $array = json_decode($json, TRUE);
+            foreach ($array as $key => $value) {
+                if (empty($value)) {
+                    unset($array[$key]);
+                }
+            }
+            if (!empty($array)) {
+                if ($array['AuthenticateUserResult'] != "") {
+                    $user_details = $this->db->get_where("customer_master", array("user_id" => $array['AuthenticateUserResult']))->row();
+
+                    if (!empty($user_details)) {
+                        $curl = curl_init();
+                        curl_setopt_array($curl, array(
+                            CURLOPT_URL => "https://secure.membershipsoftware.org/tiadasecure/api/GetMemberInfo/?securityKey=4A17DC6DF22F45B7AAF5A0554FD447&memberkey=" . $array['AuthenticateUserResult'],
+                            CURLOPT_RETURNTRANSFER => true,
+                            CURLOPT_ENCODING => "",
+                            CURLOPT_MAXREDIRS => 10,
+                            CURLOPT_TIMEOUT => 0,
+                            CURLOPT_FOLLOWLOCATION => true,
+                            CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+                            CURLOPT_CUSTOMREQUEST => "GET"
+                        ));
+                        $response = curl_exec($curl);
+                        curl_close($curl);
+                        $xml_1 = simplexml_load_string($response);
+                        $json_1 = json_encode($xml_1);
+                        $member_array = json_decode($json_1, TRUE);
+
+                        foreach ($member_array as $key => $value) {
+                            if (empty($value)) {
+                                unset($member_array[$key]);
+                            }
+                        }
+
+                        if (!empty($member_array)) {
+                            $set_update_array = array(
+                                "user_id" => $array['AuthenticateUserResult'],
+                                'first_name' => $member_array['FirstName'],
+                                'last_name' => $member_array['LastName'],
+                                'email' => $member_array['EmailAddress'],
+                                'address' => $member_array['Addresses']['MemberAddress']['AddressLine1'],
+                                'address_cont' => $member_array['Addresses']['MemberAddress']['AddressType'],
+                                'city' => $member_array['Addresses']['MemberAddress']['City'],
+                                'state' => $member_array['Addresses']['MemberAddress']['StateProvince'],
+                                'country' => $member_array['Addresses']['MemberAddress']['Country'],
+                                'phone' => $member_array['Phones']['MemberPhone'][0]['PhoneNumber'],
+                                'website' => isset($member_array['WebsiteURL']) ? $member_array['WebsiteURL'] : '',
+                                'customer_type' => $member_array['MemberType'],
+                                'customer_type_id' => $member_array['SecurityGroups']['MemberGroup']['GroupKey'],
+                                'member_status' => "Tiada-Member",
+                            );
+                            $this->db->update("customer_master", $set_update_array, array("cust_id" => $user_details->cust_id));
+                        }
+                        $session = array(
+                            'cid' => $user_details->cust_id,
+                            'cname' => $user_details->first_name,
+                            'fullname' => $user_details->first_name . " " . $user_details->last_name,
+                            'email' => $user_details->email,
+                            'userType' => 'user'
+                        );
+                        $this->session->set_userdata($session);
+                        redirect('home');
+                    } else {
+                        $curl = curl_init();
+                        curl_setopt_array($curl, array(
+                            CURLOPT_URL => "https://secure.membershipsoftware.org/tiadasecure/api/GetMemberInfo/?securityKey=4A17DC6DF22F45B7AAF5A0554FD447&memberkey=" . $array['AuthenticateUserResult'],
+                            CURLOPT_RETURNTRANSFER => true,
+                            CURLOPT_ENCODING => "",
+                            CURLOPT_MAXREDIRS => 10,
+                            CURLOPT_TIMEOUT => 0,
+                            CURLOPT_FOLLOWLOCATION => true,
+                            CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+                            CURLOPT_CUSTOMREQUEST => "GET"
+                        ));
+                        $response = curl_exec($curl);
+                        curl_close($curl);
+                        $xml_1 = simplexml_load_string($response);
+                        $json_1 = json_encode($xml_1);
+                        $member_array = json_decode($json_1, TRUE);
+
+                        foreach ($member_array as $key => $value) {
+                            if (empty($value)) {
+                                unset($member_array[$key]);
+                            }
+                        }
+
+                        if (!empty($member_array)) {
+                            $or_where = '(email = "' . trim($member_array['EmailAddress']) . '")';
+                            $this->db->where($or_where);
+                            $customer = $this->db->get('customer_master');
+                            if ($customer->num_rows() > 0) {
+                                $user_details = $customer->row();
+                                $set_update_array = array(
+                                    "user_id" => $array['AuthenticateUserResult'],
+                                    'first_name' => $member_array['FirstName'],
+                                    'last_name' => $member_array['LastName'],
+                                    'email' => $member_array['EmailAddress'],
+                                    'address' => $member_array['Addresses']['MemberAddress']['AddressLine1'],
+                                    'address_cont' => $member_array['Addresses']['MemberAddress']['AddressType'],
+                                    'city' => $member_array['Addresses']['MemberAddress']['City'],
+                                    'state' => $member_array['Addresses']['MemberAddress']['StateProvince'],
+                                    'country' => $member_array['Addresses']['MemberAddress']['Country'],
+                                    'phone' => $member_array['Phones']['MemberPhone'][0]['PhoneNumber'],
+                                    'website' => isset($member_array['WebsiteURL']) ? $member_array['WebsiteURL'] : '',
+                                    'customer_type' => $member_array['MemberType'],
+                                    'customer_type_id' => $member_array['SecurityGroups']['MemberGroup']['GroupKey'],
+                                    'member_status' => "Tiada-Member"
+                                );
+                                $this->db->update("customer_master", $set_update_array, array("cust_id" => $user_details->cust_id));
+
+                                $session = array(
+                                    'cid' => $user_details->cust_id,
+                                    'cname' => $user_details->first_name,
+                                    'fullname' => $user_details->first_name . " " . $user_details->last_name,
+                                    'email' => $user_details->email,
+                                    'userType' => 'user'
+                                );
+                                $this->session->set_userdata($session);
+                                redirect('home');
+                            } else {
+                                $this->db->order_by("cust_id", "desc");
+                                $row_data = $this->db->get("customer_master")->row();
+                                if (!empty($row_data)) {
+                                    $reg_id = $row_data->cust_id;
+                                    $register_id = date("Y") . '-20' . $reg_id;
+                                } else {
+                                    $register_id = date("Y") . '-200';
+                                }
+                                $set = array(
+                                    "register_id" => $register_id,
+                                    "user_id" => $array['AuthenticateUserResult'],
+                                    'first_name' => $member_array['FirstName'],
+                                    'last_name' => $member_array['LastName'],
+                                    'email' => $member_array['EmailAddress'],
+                                    'password' => base64_encode(1234),
+                                    'address' => $member_array['Addresses']['MemberAddress']['AddressLine1'],
+                                    'address_cont' => $member_array['Addresses']['MemberAddress']['AddressType'],
+                                    'city' => $member_array['Addresses']['MemberAddress']['City'],
+                                    'state' => $member_array['Addresses']['MemberAddress']['StateProvince'],
+                                    'country' => $member_array['Addresses']['MemberAddress']['Country'],
+                                    'phone' => $member_array['Phones']['MemberPhone'][0]['PhoneNumber'],
+                                    'website' => isset($member_array['WebsiteURL']) ? $member_array['WebsiteURL'] : '',
+                                    'customer_type' => $member_array['MemberType'],
+                                    'customer_type_id' => $member_array['SecurityGroups']['MemberGroup']['GroupKey'],
+                                    'member_status' => "Tiada-Member",
+                                    'register_date' => date("Y-m-d h:i")
+                                );
+                                $this->db->insert("customer_master", $set);
+                                $cust_id = $this->db->insert_id();
+                                $user_details = $this->db->get_where("customer_master", array("cust_id" => $cust_id))->row();
+                                if (!empty($user_details)) {
+                                    $session = array(
+                                        'cid' => $user_details->cust_id,
+                                        'cname' => $user_details->first_name,
+                                        'fullname' => $user_details->first_name . " " . $user_details->last_name,
+                                        'email' => $user_details->email,
+                                        'userType' => 'user'
+                                    );
+                                    $this->session->set_userdata($session);
+                                    redirect('home');
+                                }
+                            }
+                        } else {
+                            redirect('login');
+                        }
+                    }
+                } else {
+                    redirect('login');
+                }
+            } else {
+                redirect('login');
+            }
+        } else {
+            redirect('login');
+        }
+    }
+
 }
--- application/views/login.php
+++ application/views/login.php
@@ -3,11 +3,33 @@
     <div class="container container-fullscreen">
         <div class="text-middle">
             <div class="row">
-                <div class="col-md-4 center p-40 background-white" style="border-radius: 10px; margin-top: 60px;">
+                <div class="col-md-4 col-md-offset-2 p-40 background-white" style="border-radius: 10px; margin-top: 60px;">
                     <div class="row">
                         <div class="col-md-12"> 
-                            <h4>Welcome Back!</h4>
-                            <p>Sign in Below</p>
+                            <h4>TIADA Members</h4>
+                            <form id="tiada_login-form" name="tiada_frm_login" method="post" action="<?= base_url() ?>login/tiada_authentication">
+                                <div class="form-group">
+                                    <label class="sr-only">Username</label>
+                                    <input type="text" class="form-control" id="tiada_email" name="tiada_email" placeholder="Username">
+                                    <span id="errortiada_email" style="color:red"></span>
+                                </div>
+                                <div class="form-group m-b-5">
+                                    <label class="sr-only">Password</label>
+                                    <input type="password" id="tiada_password" name="tiada_password" class="form-control" placeholder="Password">
+                                    <span id="errortiada_password" style="color:red"></span>
+                                </div>
+                                <div class="text-left form-group">
+                                    <button type="submit" id="tiada_btn_login" class="btn btn-primary">Login</button>
+                                </div>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-4  p-40 background-white" style="border-radius: 10px; margin-top: 60px; margin-left: 10px;">
+                    <div class="row">
+                        <div class="col-md-12"> 
+                            <h4>Non Members</h4>
+<!--                            <p>Sign in Below</p>-->
                             <?php
                             echo ($this->session->flashdata('msg')) ? $this->session->flashdata('msg') : '';
                             ?> 
@@ -29,7 +51,7 @@
                                     <button type="submit" id="btn_login" class="btn btn-primary">Login</button>
                                 </div>
                             </form>
-                            <h4 style="margin-bottom: 0px;"><a href="https://www.txiada.org/login.asp?redirectURL=https://yourconference.live/tiadaannualconference/login/authenticate">Login with TIADA</a></h4>
+                            <!--                            <h4 style="margin-bottom: 0px;"><a href="https://www.txiada.org/login.asp?redirectURL=https://yourconference.live/tiadaannualconference/login/authenticate">Login with TIADA</a></h4>-->
                         </div>
                         <!--                        <div class="col-md-6">
                                                     <h4>Register Now!</h4>
@@ -51,6 +73,19 @@
                 return false;
             } else if ($("#password").val() == "") {
                 $("#errorpassword").text("Please Enter Password").fadeIn('slow').fadeOut(5000);
+                return false;
+            } else {
+                return true; //submit form
+            }
+            return false; //Prevent form to submitting
+        });
+
+        $("#tiada_btn_login").on("click", function () {
+            if ($("#tiada_email").val().trim() == "") {
+                $("#errortiada_email").text("Please Enter Username").fadeIn('slow').fadeOut(5000);
+                return false;
+            } else if ($("#tiada_password").val() == "") {
+                $("#errortiada_password").text("Please Enter Password").fadeIn('slow').fadeOut(5000);
                 return false;
             } else {
                 return true; //submit form
