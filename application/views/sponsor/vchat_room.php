8319d33e35433b415327b89a9c438470e1b7d67f
athulak <athullive@gmail.com>
2020-08-16 19:51:38 +0200

    V-Chat rooms for sponsor booths

---
+++ application/views/sponsor/vchat_room.php
@@ -0,0 +1,93 @@
+
+<link href="<?= base_url() ?>assets/css/private-sessions.css?v=<?=rand(1, 100)?>" rel="stylesheet">
+
+<main role="main" class="container text-center">
+    <div>
+        <h1>
+            <?=$sponsor->company_name?>
+        </h1>
+        <small class="lead"><?=$sponsor->about?></small>
+    </div>
+</main>
+
+<div class="row m-t-20 camera-feeds">
+
+    <div class="col-md-3">
+        <video id="localVideo" autoplay muted playsinline width="100%"></video>
+        <span class="name-tag">You</span>
+        <!-- <div class="soundbar"><span class="currentVolume"></span></div> -->
+    </div>
+
+    <!--    <div class="col-md-12">-->
+    <!--        <div class="feed-control-icons m-t-15 m-b-15">-->
+    <!--            <div class="mute-mic-btn m-b-20">-->
+    <!--                <i class="fa fa-microphone-slash fa-3x mute-mic-btn-icon" aria-hidden="true" style="color:#ff422b;"></i>-->
+    <!--                <small>Mute</small>-->
+    <!--            </div>-->
+    <!--            <div class="share-screen-btn">-->
+    <!--                <i class="fa fa-desktop fa-3x share-screen-btn-icon" aria-hidden="true" style="color:#6f8de3;"></i>-->
+    <!--                <small>Share Screen</small>-->
+    <!--            </div>-->
+    <!--        </div>-->
+    <!--    </div>-->
+</div>
+
+
+<script>
+    var round_table_id = <?=$sponsor->sponsors_id?>;
+    var atttendee_name = "<?= $this->session->userdata('fullname') ?>";
+    var attendee_id = "<?= $this->session->userdata('cid') ?>";
+</script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/socket.io/2.3.0/socket.io.js"></script>
+<script src="<?= base_url() ?>assets/js/sponsor-vchat-room.js?v=<?=rand(1, 100)?>"></script>
+<script type="text/javascript">
+    pageReady();
+</script>
+<div class="modal fade" id="push_notification" tabindex="-1" role="modal" aria-labelledby="modal-label" aria-hidden="true" style="display: none; text-align: left; right: unset;">
+    <input type="hidden" id="push_notification_id" value="">
+    <div class="modal-dialog">
+        <div class="modal-content" style="border: 1px solid #ae0201;">
+            <div class="modal-body">
+                <div class="row" style="padding-top: 10px; padding-bottom: 20px;">
+                    <div class="col-sm-12">
+                        <div style="color:#ae0201; font-size: 16px; font-weight: 800; " id="push_notification_message"></div>
+                    </div>
+                </div>
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="close" style="padding: 10px; color: #fff; background-color: #ae0201; opacity: 1;" data-dismiss="modal" aria-hidden="true">Close</button>
+            </div>
+        </div>
+    </div>
+</div>
+<script type="text/javascript">
+    $(document).ready(function () {
+        push_notification_admin();
+        setInterval(push_notification_admin, 3000);
+        function push_notification_admin()
+        {
+            var push_notification_id = $("#push_notification_id").val();
+
+            $.ajax({
+                url: "<?= base_url() ?>push_notification/get_push_notification_admin",
+                type: "post",
+                dataType: "json",
+                success: function (data) {
+                    if (data.status == "success") {
+                        if (push_notification_id == "0") {
+                            $("#push_notification_id").val(data.result.push_notification_id);
+                        }
+                        if (push_notification_id != data.result.push_notification_id) {
+                            $("#push_notification_id").val(data.result.push_notification_id);
+                            $('#push_notification').modal('show');
+                            $("#push_notification_message").text(data.result.message);
+                        }
+                    } else {
+                        $('#push_notification').modal('hide');
+                    }
+                }
+            });
+        }
+
+    });
+</script>
--- application/views/view_sponsor.php
+++ application/views/view_sponsor.php
@@ -13,6 +13,7 @@ $sponsor_cover = ($sponsor->sponsor_cover == '') ? 'tiada_default_cover.jpg' : $
 
     <!-- Main jumbotron for a primary marketing message or call to action -->
     <div class="jumbotron" style="background-image: url(<?= base_url() ?>uploads/sponsors/<?= $sponsor_cover ?>?v=<?= rand(1, 100) ?>);background-size: 1930px;background-repeat: no-repeat;background-position: center;height: 600px;background-color: #272f31;">
+        <span class="pull-right fishbowl" style="cursor: pointer;"><img src="<?= base_url() ?>front_assets/sponsor/images/fishbowl.png" style="width: 165px;margin-top: 30px;"><br><span style="color: white;font-size: 15px;">click to leave your card</span></span>
     </div>
 
     <div class="container">
@@ -142,7 +143,7 @@ $sponsor_cover = ($sponsor->sponsor_cover == '') ? 'tiada_default_cover.jpg' : $
             </div>
 
         </div>
-
+        <a target="_blank" href="<?= base_url() ?>sponsor-admin/VchatRoom/join/<?=$sponsor->sponsors_id?>"><img src="<?= base_url() ?>front_assets/sponsor/images/tx_flag_75.png"></a>
         <hr>
 
     </div> <!-- /container -->
